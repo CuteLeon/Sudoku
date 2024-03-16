0da0cc63fed91a470a98508f0a53d21dea76338c
Leon <Leon.Engineer@hotmail.com>
2024-03-16 20:19:03 +0800

    1

--- Sudoku/Cell.Designer.cs
+++ Sudoku/Cell.Designer.cs
@@ -29,12 +29,15 @@ partial class Cell
     private void InitializeComponent()
     {
         this.MainProbsPanel = new TableLayoutPanel();
+        this.MainLabel = new Label();
+        this.MainProbsPanel.SuspendLayout();
         this.SuspendLayout();
         // 
         // MainProbsPanel
         // 
         this.MainProbsPanel.ColumnCount = 1;
         this.MainProbsPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+        this.MainProbsPanel.Controls.Add(this.MainLabel, 0, 0);
         this.MainProbsPanel.Dock = DockStyle.Fill;
         this.MainProbsPanel.Location = new Point(0, 0);
         this.MainProbsPanel.Name = "MainProbsPanel";
@@ -43,16 +46,28 @@ partial class Cell
         this.MainProbsPanel.Size = new Size(150, 150);
         this.MainProbsPanel.TabIndex = 0;
         // 
+        // MainLabel
+        // 
+        this.MainLabel.Dock = DockStyle.Fill;
+        this.MainLabel.Font = new Font("Segoe UI", 36F, FontStyle.Bold, GraphicsUnit.Point, 0);
+        this.MainLabel.Location = new Point(3, 0);
+        this.MainLabel.Name = "MainLabel";
+        this.MainLabel.Size = new Size(144, 150);
+        this.MainLabel.TabIndex = 0;
+        this.MainLabel.TextAlign = ContentAlignment.MiddleCenter;
+        // 
         // Cell
         // 
         this.AutoScaleDimensions = new SizeF(8F, 20F);
         this.AutoScaleMode = AutoScaleMode.Font;
         this.Controls.Add(this.MainProbsPanel);
         this.Name = "Cell";
+        this.MainProbsPanel.ResumeLayout(false);
         this.ResumeLayout(false);
     }
 
     #endregion
 
     private TableLayoutPanel MainProbsPanel;
+    private Label MainLabel;
 }
