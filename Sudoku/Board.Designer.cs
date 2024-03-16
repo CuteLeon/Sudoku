5c5e82568f815e5bda5bd4ccb403bd9860a8ea3c
Leon <Leon.Engineer@hotmail.com>
2024-03-16 19:44:14 +0800

    Layout board;

--- Sudoku/Board.Designer.cs
+++ Sudoku/Board.Designer.cs
@@ -28,17 +28,34 @@ partial class Board
     /// </summary>
     private void InitializeComponent()
     {
+        this.MainBoxesPanel = new TableLayoutPanel();
         this.SuspendLayout();
+        // 
+        // MainBoxesPanel
+        // 
+        this.MainBoxesPanel.CellBorderStyle = TableLayoutPanelCellBorderStyle.Single;
+        this.MainBoxesPanel.ColumnCount = 1;
+        this.MainBoxesPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+        this.MainBoxesPanel.Dock = DockStyle.Fill;
+        this.MainBoxesPanel.Location = new Point(0, 0);
+        this.MainBoxesPanel.Name = "MainBoxesPanel";
+        this.MainBoxesPanel.RowCount = 1;
+        this.MainBoxesPanel.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+        this.MainBoxesPanel.Size = new Size(602, 602);
+        this.MainBoxesPanel.TabIndex = 0;
         // 
         // Board
         // 
         this.AutoScaleDimensions = new SizeF(8F, 20F);
         this.AutoScaleMode = AutoScaleMode.Font;
         this.BackColor = Color.White;
+        this.Controls.Add(this.MainBoxesPanel);
         this.Name = "Board";
         this.Size = new Size(602, 602);
         this.ResumeLayout(false);
     }
 
     #endregion
+
+    private TableLayoutPanel MainBoxesPanel;
 }
--- Sudoku/Board.cs
+++ Sudoku/Board.cs
@@ -1,18 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿namespace Sudoku;
 
-namespace Sudoku;
 public partial class Board : UserControl
 {
     public Board()
     {
         InitializeComponent();
+
+        this.MainBoxesPanel.RowStyles.Clear();
+        this.MainBoxesPanel.RowCount = 0;
+        this.MainBoxesPanel.ColumnStyles.Clear();
+        this.MainBoxesPanel.ColumnCount = 0;
+
+        var rowPercent = 1.0f / Contracts.BoxRowCount;
+        for (int i = 0; i < Contracts.BoxRowCount; i++)
+            this.MainBoxesPanel.RowStyles.Add(new RowStyle(SizeType.Percent, rowPercent));
+        this.MainBoxesPanel.RowCount = Contracts.BoxRowCount;
+
+        var columnPercent = 1.0f / Contracts.BoxColumnCount;
+        for (int i = 0; i < Contracts.BoxColumnCount; i++)
+            this.MainBoxesPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, columnPercent));
+        this.MainBoxesPanel.ColumnCount = Contracts.BoxColumnCount;
     }
 }
