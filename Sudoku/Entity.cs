52a2aa92d554e252a0e8e202f08186fb9868f048
Leon <Leon.Engineer@hotmail.com>
2024-03-17 12:38:54 +0800

    Entity;

---
+++ Sudoku/Entity.cs
@@ -0,0 +1,24 @@
+﻿namespace Sudoku;
+
+public class BoardEntity
+{
+    public BoxEntity[,] Boxes { get; init; } = new BoxEntity[Contracts.Size, Contracts.Size];
+}
+
+public class BoxEntity
+{
+    public int BoxRow { get; set; }
+
+    public int BoxColumn { get; set; }
+
+    public CellEntity[,] Cells { get; init; } = new CellEntity[Contracts.Size, Contracts.Size];
+}
+
+public class CellEntity
+{
+    public int CellRow { get; set; }
+
+    public int CellColumn { get; set; }
+
+    public int? Number { get; set; }
+}
