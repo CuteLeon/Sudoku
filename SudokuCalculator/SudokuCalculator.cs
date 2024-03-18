61f441210c26edbd42ca63f975132e862da16aae
Leon <Leon.Engineer@hotmail.com>
2024-03-18 17:03:31 +0800

    add CalculateProbableSet;

--- SudokuCalculator/SudokuCalculator.cs
+++ SudokuCalculator/SudokuCalculator.cs
@@ -6,6 +6,25 @@ public class SudokuCalculator
 {
     public void Calculate(FrozenDictionary<CellLocation, CellEntity> cells)
     {
+        var size = 3;
+        this.CalculateProbableSet(cells, size);
+    }
 
+    protected void CalculateProbableSet(FrozenDictionary<CellLocation, CellEntity> cells, int size)
+    {
+        foreach (var pair in cells)
+        {
+            var cellLocation = pair.Key;
+            var cellEntity = pair.Value;
+            var probableSet = Enumerable.Range(1, size * size).ToHashSet();
+
+            for (var boxColumn = 0; boxColumn < size; boxColumn++)
+            {
+            }
+
+            cellEntity.ProbableSet.Clear();
+            foreach (var probable in probableSet)
+                cellEntity.ProbableSet.Add(probable);
+        }
     }
 }
