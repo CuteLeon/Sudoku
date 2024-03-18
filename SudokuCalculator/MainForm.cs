0124298eed84c97cadfec0a7c9ed325dcdef4662
Leon <Leon.Engineer@hotmail.com>
2024-03-18 13:13:37 +0800

    Auto size;

--- SudokuCalculator/MainForm.cs
+++ SudokuCalculator/MainForm.cs
@@ -5,5 +5,10 @@ public partial class MainForm : Form
     public MainForm()
     {
         InitializeComponent();
+
+        var offsetWidth = this.Width - this.BoardLayoutPanel.Width;
+        var offsetHeight = this.Height - this.BoardLayoutPanel.Height;
+        this.Width = 300 + offsetWidth;
+        this.Height = 300 + offsetHeight;
     }
 }
